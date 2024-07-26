ae0cc9cbcf7b21b2115b51fe78b763542c67eb03
Wooshow <>
2024-07-26 21:18:28 +0800

    add --config flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup openvpn /data/conf/openvpn/openvpn.ovpn --auth-user-pass /data/conf/openvpn/pass.txt >> /data/log/ovpn.log &
+nohup openvpn --config /data/conf/openvpn/openvpn.ovpn --auth-user-pass /data/conf/openvpn/pass.txt >> /data/log/ovpn.log &
 sleep 8
 ssserver -c /data/conf/shadowsocks/config.json
